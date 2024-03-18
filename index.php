a81627c2a712c397c38893907ad0c9e548276133
Romain Thillaye du Boullay <rduboullay@gmail.com>
2024-03-18 09:17:09 +0100

    register connexion

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <h1>Bienvenue sur la page d'enchères</h1>
+
+    <a href="./connexion.php">Connexion</a>
+    <a href="./inscription.php">Inscription</a>
+
+</body>
+
+</html>
